14b31716431297494e4a844463b5f3d2cd9cd681
VaishnaviP666 <122201508+VaishnaviP666@users.noreply.github.com>
2023-05-23 22:35:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function volume_sphere() {
     //Write your code here
-  
+  let Volume=(3.14*4*Radius*Radius*Radius)/3
+
+	return  Volume;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
